bd946c1ee979d966f20d3554bb68507a7c9314ff
alphineandre <alphyandrechauke@gmail.com>
2025-02-17 09:16:12 +0200

    More tests added

--- test/calculatorapp/CalculatorTest.java
+++ test/calculatorapp/CalculatorTest.java
@@ -8,26 +8,58 @@ public class CalculatorTest {
 
     @Test
     public void testAddition() {
-        assertEquals(8, calculator.add(5, 3));
+        assertEquals(10, calculator.add(7, 3));
+        assertEquals(-2, calculator.add(-5, 3));
     }
 
     @Test
     public void testSubtraction() {
-        assertEquals(2, calculator.subtract(5, 3));
+        assertEquals(4, calculator.subtract(7, 3));
+        assertEquals(-8, calculator.subtract(-5, 3));
     }
 
     @Test
     public void testMultiplication() {
-        assertEquals(15, calculator.multiply(5, 3));
+        assertEquals(21, calculator.multiply(7, 3));
+        assertEquals(-15, calculator.multiply(-5, 3));
     }
 
     @Test
     public void testDivision() {
         assertEquals(2, calculator.divide(6, 3));
+        assertEquals(-2.5, calculator.divide(-5, 2));
     }
 
     @Test
     public void testDivisionByZero() {
         assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+    }
+
+    @Test
+    public void testModulus() {
+        assertEquals(1, calculator.modulus(7, 3));
+        assertEquals(0, calculator.modulus(6, 3));
+    }
+
+    @Test
+    public void testModulusByZero() {
+        assertThrows(ArithmeticException.class, () -> calculator.modulus(7, 0));
+    }
+
+    @Test
+    public void testPower() {
+        assertEquals(8, calculator.power(2, 3));
+        assertEquals(1, calculator.power(5, 0));
+    }
+
+    @Test
+    public void testSquareRoot() {
+        assertEquals(3, calculator.squareRoot(9));
+        assertEquals(5, calculator.squareRoot(25));
+    }
+
+    @Test
+    public void testSquareRootNegative() {
+        assertThrows(ArithmeticException.class, () -> calculator.squareRoot(-9));
     }
 }

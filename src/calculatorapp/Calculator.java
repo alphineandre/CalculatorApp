3d4bf190cbf6459bfa2247ace42afa713d165bc5
alphineandre <alphyandrechauke@gmail.com>
2025-02-17 09:15:29 +0200

    more methods added

--- src/calculatorapp/Calculator.java
+++ src/calculatorapp/Calculator.java
@@ -19,4 +19,22 @@ public class Calculator {
         }
         return a / b;
     }
+
+    public double modulus(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot calculate modulus with zero");
+        }
+        return a % b;
+    }
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public double squareRoot(double a) {
+        if (a < 0) {
+            throw new ArithmeticException("Cannot calculate square root of a negative number");
+        }
+        return Math.sqrt(a);
+    }
 }
